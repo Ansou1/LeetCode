91b966fad9caa4ededf54da10eb6c59183f63aac
simon <simondaguenet81@gmail.com>
2019-03-11 00:01:21 +0900

    EX4

--- src/com/ansou/leetcode/Main.java
+++ src/com/ansou/leetcode/Main.java
@@ -2,6 +2,9 @@ package com.ansou.leetcode;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 
 public class Main {
 
@@ -67,12 +70,78 @@ public class Main {
     }
 
     /* 4. Median of Two Sorted Arrays */
+    public static double median(int[] m) {
+        int middle = m.length/2;
+        if (m.length % 2 == 1) {
+            return m[middle];
+        } else {
+            return (m[middle-1] + m[middle]) / 2.0;
+        }
+    }
+
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] result = IntStream.concat(Arrays.stream(nums1), Arrays.stream(nums2)).sorted().toArray();
+        return median(result);
+    }
+
+
+
+    public static double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int mid = (m+n)/2;
+        int mod = (m+n)%2;
+        int count = 0;
+        int x = 0;
+        int y = 0;
+        double firstNum = 0;
+        double secondNum = 0;
+
+        while (count <= mid) {
+            if (x == m) {
+                if (count == mid-1)
+                    firstNum = nums2[y];
+                secondNum = nums2[y];
+                y++;
+            } else if (y == n) {
+                if (count == mid-1)
+                    firstNum = nums1[x];
+                secondNum = nums1[x];
+                x++;
+            } else if (nums1[x]<=nums2[y]) {
+                if (count == mid-1)
+                    firstNum = nums1[x];
+                secondNum = nums1[x];
+                x++;
+            } else {
+                if (count == mid-1)
+                    firstNum = nums2[y];
+                secondNum = nums2[y];
+                y++;
+            }
+            count++;
+        }
+        if (mod>0){
+            return secondNum;
+        }
+        return (firstNum+secondNum)/2;
     }
 
     public static void main(String[] args) {
         int[] nums1 = {1, 2};
         int[] nums2 = {3, 4};
         System.out.println(findMedianSortedArrays(nums1, nums2));
+
+        int[] nums3 = {1, 3};
+        int[] nums4 = {2};
+        System.out.println(findMedianSortedArrays(nums3, nums4));
+
+        int[] nums5 = {2};
+        int[] nums6 = {};
+        System.out.println(findMedianSortedArrays(nums5, nums6));
+
+        int[] nums7 = {3};
+        int[] nums8 = {-2, -1};
+        System.out.println(findMedianSortedArrays(nums7, nums8));
     }
 }
